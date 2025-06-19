a34f8228fe36827e780cc61770963410f99fb91b
developerdesigner18 <developerdesigner18@gmail.com>
2025-06-19 18:01:02 +0530

    Fix:Google-TTS

--- utils/googleTTSClient.js
+++ utils/googleTTSClient.js
@@ -2,7 +2,7 @@ const textToSpeech = require("@google-cloud/text-to-speech");
 const path = require("path");
 
 const client = new textToSpeech.TextToSpeechClient({
-  keyFilename: path.join(__dirname, "../google-tts-key.json"),
+  keyFilename: path.join(__dirname, "../config/google-tts-key.json"),
 });
 
 module.exports = client;
